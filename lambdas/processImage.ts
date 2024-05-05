733322795486231ea53a55ad2246eca8ff66450f
MingJu Sun <20104599@mail.wit.ie>
2024-05-05 08:08:51 +0100

    update processImage lambdas and the stack

--- lambdas/processImage.ts
+++ lambdas/processImage.ts
@@ -7,35 +7,57 @@ import {
   S3Client,
   PutObjectCommand,
 } from "@aws-sdk/client-s3";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const s3 = new S3Client();
+const ddbDocClient = createDDbDocClient();
 
 export const handler: SQSHandler = async (event) => {
   console.log("Event ", JSON.stringify(event));
   for (const record of event.Records) {
-    const recordBody = JSON.parse(record.body);  // Parse SQS message
-    const snsMessage = JSON.parse(recordBody.Message); // Parse SNS message
-
-    if (snsMessage.Records) {
-      console.log("Record body ", JSON.stringify(snsMessage));
-      for (const messageRecord of snsMessage.Records) {
+     const recordMessage = JSON.parse(JSON.parse(record.body).Message);
+        if (recordMessage.Records) {
+            for (const messageRecord of recordMessage.Records) {
         const s3e = messageRecord.s3;
         const srcBucket = s3e.bucket.name;
         // Object key may have spaces or unicode non-ASCII characters.
         const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        let origimage = null;
-        try {
-          // Download the image from the S3 source bucket.
-          const params: GetObjectCommandInput = {
-            Bucket: srcBucket,
-            Key: srcKey,
-          };
-          origimage = await s3.send(new GetObjectCommand(params));
-          // Process the image ......
-        } catch (error) {
-          console.log(error);
+        const typeMatch = srcKey.match(/\.([^.]*)$/);
+                if (!typeMatch) {
+                    console.log("No image type.");
+                    throw new Error("No image type. ");
+                }
+
+                const imageType = typeMatch[1].toLowerCase();
+                if (imageType != "jpeg" && imageType != "png") {
+                    console.log(`Unsupported image type: ${imageType}`);
+                    throw new Error("Unsupported image type: ${imageType}. ");
+                }
+
+                const putCommand = new PutCommand({
+                    TableName: "Images",
+                    Item: {
+                        ImageName: srcKey,
+                    },
+                });
+
+                await ddbDocClient.send(putCommand);
+            }
         }
-      }
     }
-  }
 };
+
+function createDDbDocClient() {
+    const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+    const marshallOptions = {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+        wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -8,6 +8,8 @@ import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as iam from "aws-cdk-lib/aws-iam";
+import { Duration } from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -23,86 +25,116 @@ export class EDAAppStack extends cdk.Stack {
     });
 
     // Integration infrastructure
-  const mailerQ = new sqs.Queue(this, "mailer-queue", {
-      receiveMessageWaitTime: cdk.Duration.seconds(10),
+  const topic1 = new sns.Topic(this, "topic1", {
+      displayName: "topic 1",
     });
-
-  const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
-      receiveMessageWaitTime: cdk.Duration.seconds(10),
-    });
-
-    const newImageTopic = new sns.Topic(this, "NewImageTopic", {
-      displayName: "New Image topic",
-    }); 
-    // Lambda functions
-
-    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      memorySize: 1024,
-      timeout: cdk.Duration.seconds(3),
-      entry: `${__dirname}/../lambdas/mailer.ts`,
-    });
-
-    const processImageFn = new lambdanode.NodejsFunction(
-      this,
-      "ProcessImageFn",
-      {
-        runtime: lambda.Runtime.NODEJS_18_X,
-        entry: `${__dirname}/../lambdas/processImage.ts`,
-        timeout: cdk.Duration.seconds(15),
-        memorySize: 128,
-      }
-    );
-
-   // S3 --> SQS
     imagesBucket.addEventNotification(
         s3.EventType.OBJECT_CREATED,
-        new s3n.SnsDestination(newImageTopic)  // Changed
+        new s3n.SnsDestination(topic1)
     );
 
-    newImageTopic.addSubscription(
-      new subs.SqsSubscription(imageProcessQueue)
+    const confirmationMailerFn = new lambdanode.NodejsFunction(
+        this,
+        "ConfirmationMailerFn",
+        {
+          runtime: lambda.Runtime.NODEJS_18_X,
+          entry: `${__dirname}/../lambdas/confirmationMailer.ts`,
+          timeout: cdk.Duration.seconds(15),
+          memorySize: 128,
+        }
+    );
+    topic1.addSubscription(new subs.LambdaSubscription(confirmationMailerFn));
+
+    confirmationMailerFn.addToRolePolicy(
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: [
+            "ses:SendEmail",
+            "ses:SendRawEmail",
+            "ses:SendTemplatedEmail",
+          ],
+          resources: ["*"],
+        })
     );
 
-    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
-
-   // SQS --> Lambda
-    const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
-      batchSize: 5,
-      maxBatchingWindow: cdk.Duration.seconds(10),
+    const DLQ = new sqs.Queue(this, "bad-orders-q", {
+      retentionPeriod: Duration.minutes(30),
     });
 
-    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
-      batchSize: 5,
-      maxBatchingWindow: cdk.Duration.seconds(10),
-    }); 
-
-    
-
-    processImageFn.addEventSource(newImageEventSource);
-
-    mailerFn.addEventSource(newImageMailEventSource);
-
-    mailerFn.addToRolePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        actions: [
-          "ses:SendEmail",
-          "ses:SendRawEmail",
-          "ses:SendTemplatedEmail",
-        ],
-        resources: ["*"],
-      })
+    const rejectionMailerFn = new lambdanode.NodejsFunction(
+        this,
+        "RejectionMailerFn",
+        {
+          runtime: lambda.Runtime.NODEJS_18_X,
+          entry: `${__dirname}/../lambdas/rejectionMailer.ts`,
+          timeout: cdk.Duration.seconds(15),
+          memorySize: 128,
+        }
+    );
+    rejectionMailerFn.addEventSource(
+        new events.SqsEventSource(DLQ, {
+          batchSize: 5,
+          maxBatchingWindow: cdk.Duration.seconds(10),
+        })
+    );
+    rejectionMailerFn.addToRolePolicy(
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: [
+            "ses:SendEmail",
+            "ses:SendRawEmail",
+            "ses:SendTemplatedEmail",
+          ],
+          resources: ["*"],
+        })
     );
 
-    // Permissions
 
+    const Queue = new sqs.Queue(this, "orders-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+      deadLetterQueue: {
+        queue: DLQ,
+        maxReceiveCount: 2,
+      },
+    });
+
+    topic1.addSubscription(new subs.SqsSubscription(Queue));
+
+
+    const processImageFn = new lambdanode.NodejsFunction(
+        this,
+        "ProcessImageFn",
+        {
+          runtime: lambda.Runtime.NODEJS_18_X,
+          entry: `${__dirname}/../lambdas/processImage.ts`,
+          timeout: cdk.Duration.seconds(15),
+          memorySize: 128,
+        }
+    );
     imagesBucket.grantRead(processImageFn);
+    processImageFn.addEventSource(
+        new events.SqsEventSource(Queue, {
+          batchSize: 5,
+          maxBatchingWindow: cdk.Duration.seconds(10),
+        })
+    );
 
-    // Output
-    
+    const imagesTable = new dynamodb.Table(this, "imagesTable", {
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        partitionKey: { name: "ImageName", type: dynamodb.AttributeType.STRING },
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+        tableName: "Images",
+      });
+
+    imagesTable.grantReadWriteData(processImageFn);
+
     new cdk.CfnOutput(this, "bucketName", {
-      value: imagesBucket.bucketName,
+        value: imagesBucket.bucketName,
     });
+    new cdk.CfnOutput(this, "topic1ARN", {
+        value: topic1.topicArn,
+    });
+
   }
 }
